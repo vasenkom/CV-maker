69d239240ebcb0984777c5951a73554bbc6d2200
vasenkom <m.vasenko@yandex.ru>
2024-08-27 22:37:35 +0200

    create a component for setting buttons

---
+++ cvMaker/src/components/settings/SettingsMain.jsx
@@ -0,0 +1,10 @@
+import { SettingsButtons } from "./Buttons";
+
+export function SettingPart() {
+  return (
+    <div id="SettingPart">
+      <SettingsButtons text="Content" id="ContentButton" image="#" />
+      <SettingsButtons text="Customize" id="CustomizeButton" image="#" />
+    </div>
+  );
+}
