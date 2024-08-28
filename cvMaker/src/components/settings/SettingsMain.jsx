f001673675263cc96980f56f48dd5b8d9b241fd4
vasenkom <m.vasenko@yandex.ru>
2024-08-28 08:31:27 +0200

    add comments and frafment structure

--- cvMaker/src/components/settings/SettingsMain.jsx
+++ cvMaker/src/components/settings/SettingsMain.jsx
@@ -1,10 +1,14 @@
 import { SettingsButtons } from "./Buttons";
 
 export function SettingPart() {
+  // Add functionality to each button here
+
   return (
     <div id="SettingPart">
-      <SettingsButtons text="Content" id="ContentButton" image="#" />
-      <SettingsButtons text="Customize" id="CustomizeButton" image="#" />
+      <>
+        <SettingsButtons text="Content" id="ContentButton" image="#" />
+        <SettingsButtons text="Customize" id="CustomizeButton" image="#" />
+      </>
     </div>
   );
 }
