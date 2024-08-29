a728e0674e81fdd199c87c90e3ff50bc08d0a063
vasenkom <m.vasenko@yandex.ru>
2024-08-29 21:31:42 +0200

    add toggle switch and add css for div so it is flex

--- cvMaker/src/components/settings/SettingsMain.jsx
+++ cvMaker/src/components/settings/SettingsMain.jsx
@@ -1,14 +1,21 @@
 import { SettingsButtons } from "./Buttons";
+import { ToggleSwitch } from "./ToggleSwitch";
 
 export function SettingPart() {
-  // Add functionality to each button here
-
   return (
     <div id="SettingPart">
-      <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "20px",
+          marginBottom: "30px",
+        }}
+      >
         <SettingsButtons text="Content" id="ContentButton" image="#" />
         <SettingsButtons text="Customize" id="CustomizeButton" image="#" />
-      </>
+      </div>
+      <ToggleSwitch />
     </div>
   );
 }
