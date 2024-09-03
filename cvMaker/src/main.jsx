8b4f6f66ba803d31256324cbe4ffca4464c92839
vasenkom <m.vasenko@yandex.ru>
2024-09-03 23:31:28 +0200

    add form to the amin file

--- cvMaker/src/main.jsx
+++ cvMaker/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SettingPart } from "./components/settings/SettingsMain";
-
+import { Forms } from "./components/forms/allForms";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div id="Application">
       <div id="Left">
+        <Forms />
         <SettingPart />
       </div>
       <div id="Right"></div>
