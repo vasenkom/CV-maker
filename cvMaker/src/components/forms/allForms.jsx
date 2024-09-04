a4b8d4f6b011df9fe91fc907b5c3881565d50c3c
vasenkom <m.vasenko@yandex.ru>
2024-09-04 15:07:33 +0200

    add input for personal info form: email, phone, address

--- cvMaker/src/components/forms/allForms.jsx
+++ cvMaker/src/components/forms/allForms.jsx
@@ -18,13 +18,44 @@ export function Forms() {
   };
 
   return (
-    <form id="personalForm">
-      <label htmlFor="name">Name:</label>
-      <PersonalForm
-        name="name"
-        value={personalInfo.name}
-        onChange={handleInputChange}
-      />
-    </form>
+    <div id="form">
+      <p
+        style={{
+          marginTop: "10px",
+          marginLeft: "15px",
+          marginRight: "15px",
+          fontSize: "17px",
+          fontWeight: "600",
+        }}
+      >
+        Personal info:
+      </p>
+      <form id="personalForm">
+        <label htmlFor="name">Name:</label>
+        <PersonalForm
+          name="name"
+          value={personalInfo.name}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="email">Email:</label>
+        <PersonalForm
+          name="email"
+          value={personalInfo.email}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="phone">Phone number:</label>
+        <PersonalForm
+          name="email"
+          value={personalInfo.email}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="address">Address:</label>
+        <PersonalForm
+          name="address"
+          value={personalInfo.address}
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
   );
 }
