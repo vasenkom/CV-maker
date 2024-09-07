ae9a6be998fd86c283702cc28d9fc5807c32a4b7
vasenkom <m.vasenko@yandex.ru>
2024-09-07 22:51:03 +0200

    add secondLine of the head with email, phone and address that will dynamically update when user input data

--- cvMaker/src/components/CV.jsx
+++ cvMaker/src/components/CV.jsx
@@ -1,7 +1,22 @@
 export function CVprototype({ personalInfo }) {
   return (
     <div id="CVprototype">
-      <h1 id="name">{personalInfo.name}</h1>
+      <div id="head">
+        <h1 id="name" style={{ paddingTop: "15px", marginBottom: "10px" }}>
+          {personalInfo.name}
+        </h1>
+        <div id="secondLine">
+          <p id="email" className="secondLineText">
+            {personalInfo.email}
+          </p>
+          <p id="phone" className="secondLineText">
+            {personalInfo.phone}
+          </p>
+          <p id="address" className="secondLineText">
+            {personalInfo.address}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
