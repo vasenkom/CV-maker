18ad1de1b5da4355353f37bfdb6272b9ca92937c
vasenkom <m.vasenko@yandex.ru>
2024-09-09 22:19:11 +0200

    move style of the form to css, add education div

--- cvMaker/src/components/forms/allForms.jsx
+++ cvMaker/src/components/forms/allForms.jsx
@@ -2,45 +2,45 @@ import { useState } from "react";
 import { PersonalForm } from "./personalForm";
 
 export function Forms({ personalInfo, handleInputChange }) {
+  function formAppers() {}
   return (
-    <div id="form">
-      <p
-        style={{
-          marginTop: "10px",
-          marginLeft: "15px",
-          marginRight: "15px",
-          fontSize: "17px",
-          fontWeight: "600",
-        }}
-      >
-        Personal info:
-      </p>
-      <form id="personalForm">
-        <label htmlFor="name">Name:</label>
-        <PersonalForm
-          name="name"
-          value={personalInfo.name}
-          onChange={handleInputChange}
-        />
-        <label htmlFor="email">Email:</label>
-        <PersonalForm
-          name="email"
-          value={personalInfo.email}
-          onChange={handleInputChange}
-        />
-        <label htmlFor="phone">Phone number:</label>
-        <PersonalForm
-          name="phone"
-          value={personalInfo.phone}
-          onChange={handleInputChange}
-        />
-        <label htmlFor="address">Address:</label>
-        <PersonalForm
-          name="address"
-          value={personalInfo.address}
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
+    <>
+      <div id="form">
+        <p className="personal-info-label">Personal info:</p>
+        <form id="personalForm">
+          <label htmlFor="name">Name:</label>
+          <PersonalForm
+            name="name"
+            value={personalInfo.name}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="email">Email:</label>
+          <PersonalForm
+            name="email"
+            value={personalInfo.email}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="phone">Phone number:</label>
+          <PersonalForm
+            name="phone"
+            value={personalInfo.phone}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="address">Address:</label>
+          <PersonalForm
+            name="address"
+            value={personalInfo.address}
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+
+      <div id="form">
+        <p className="personal-info-label">Education:</p>
+        <button id="formAppersButton" onClick={formAppers}>
+          +
+        </button>
+      </div>
+    </>
   );
 }
