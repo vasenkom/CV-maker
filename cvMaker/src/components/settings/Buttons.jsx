cdeb3b4267c1dafed45234c442c4bf4c5e070b5a
vasenkom <m.vasenko@yandex.ru>
2024-09-09 22:18:42 +0200

    move style to css

--- cvMaker/src/components/settings/Buttons.jsx
+++ cvMaker/src/components/settings/Buttons.jsx
@@ -1,17 +1,8 @@
-export function SettingsButtons({ text, id, image }) {
-  const buttonStyle = {
-    backgroundColor: "white",
-    fontSize: "20px",
-    borderRadius: "10px",
-    border: "1px solid rgb(235, 235, 235)",
-    padding: "5px 7px",
-    cursor: "pointer",
-  };
-
+export function SettingsButtons({ text, id, image, className }) {
   // Add functionality to each button here
 
   return (
-    <button className="settingButton" id={id} style={buttonStyle}>
+    <button id={id} className={className}>
       {image} {text}
     </button>
   );
