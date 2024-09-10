f243009bdc4f703bf82f398039bb00596e7d4c0b
vasenkom <m.vasenko@yandex.ru>
2024-09-10 21:20:12 +0200

    create PersonalForms component

---
+++ cvMaker/src/components/forms/PersonalForms.jsx
@@ -0,0 +1,37 @@
+import { Form } from "./form";
+
+export function PersonalForms({ personalInfo, handleInputChange }) {
+  return (
+    <>
+      <div id="form">
+        <p className="personal-info-label">Personal info:</p>
+        <form id="personalForm">
+          <label htmlFor="name">Name:</label>
+          <Form
+            name="name"
+            value={personalInfo.name}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="email">Email:</label>
+          <Form
+            name="email"
+            value={personalInfo.email}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="phone">Phone number:</label>
+          <Form
+            name="phone"
+            value={personalInfo.phone}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="address">Address:</label>
+          <Form
+            name="address"
+            value={personalInfo.address}
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+    </>
+  );
+}
