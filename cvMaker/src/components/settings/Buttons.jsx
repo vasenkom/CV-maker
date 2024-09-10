18b51a565e347d8755adc3db438c490f1814751f
vasenkom <m.vasenko@yandex.ru>
2024-09-10 21:21:20 +0200

    add onclick

--- cvMaker/src/components/settings/Buttons.jsx
+++ cvMaker/src/components/settings/Buttons.jsx
@@ -1,8 +1,8 @@
-export function SettingsButtons({ text, id, image, className }) {
+export function SettingsButtons({ text, id, image, className, onclick }) {
   // Add functionality to each button here
 
   return (
-    <button id={id} className={className}>
+    <button id={id} className={className} onClick={onclick}>
       {image} {text}
     </button>
   );
