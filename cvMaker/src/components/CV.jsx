a1bafdf786ac241b884a26268d975d4a11d9ba9f
vasenkom <m.vasenko@yandex.ru>
2024-09-10 21:19:04 +0200

    add education form in cv prototype

--- cvMaker/src/components/CV.jsx
+++ cvMaker/src/components/CV.jsx
@@ -1,22 +1,37 @@
-export function CVprototype({ personalInfo }) {
+export function CVprototype({ personalInfo, educationInfo }) {
   return (
-    <div id="CVprototype">
-      <div id="head">
-        <h1 id="name" style={{ paddingTop: "15px", marginBottom: "10px" }}>
-          {personalInfo.name}
-        </h1>
-        <div id="secondLine">
-          <p id="email" className="secondLineText">
-            {personalInfo.email}
-          </p>
-          <p id="phone" className="secondLineText">
-            {personalInfo.phone}
-          </p>
-          <p id="address" className="secondLineText">
-            {personalInfo.address}
-          </p>
+    <>
+      <div id="CVprototype">
+        <div id="head">
+          <h1 id="name" style={{ paddingTop: "15px", marginBottom: "10px" }}>
+            {personalInfo.name}
+          </h1>
+          <div id="secondLine">
+            <p id="email" className="secondLineText">
+              {personalInfo.email}
+            </p>
+            <p id="phone" className="secondLineText">
+              {personalInfo.phone}
+            </p>
+            <p id="address" className="secondLineText">
+              {personalInfo.address}
+            </p>
+          </div>
+        </div>
+        <div id="education">
+          <h2>Education</h2>
+          <div id="column">
+            <p>
+              {educationInfo.startDate} - {educationInfo.endDate}
+            </p>
+            <p>{educationInfo.location}</p>
+          </div>
+          <div id="column">
+            <p>{educationInfo.school}</p>
+            <p>{educationInfo.degree}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
