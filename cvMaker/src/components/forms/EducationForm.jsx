cba19f6c03cdca5bb0c0e45cacd2acf72dedd369
vasenkom <m.vasenko@yandex.ru>
2024-09-13 14:15:08 +0200

    add div that will be shown after the school data will be saved

--- cvMaker/src/components/forms/EducationForm.jsx
+++ cvMaker/src/components/forms/EducationForm.jsx
@@ -1,62 +1,86 @@
-import { useState } from "react";
 import { Form } from "./form";
 
-export function EducationForm({ educationInfo, handleInputChange }) {
-  const [visibleForm, setVisibleForm] = useState(false);
-  function formAppers() {
-    setVisibleForm(!visibleForm);
-    console.log("Education Add", visibleForm ? "is visible" : "is hidden");
-  }
+export function EducationForm({
+  educationList,
+  handleInputChange,
+  currentEducation,
+  saveEducationInfo,
+  visibleForm,
+  makeFormAppear,
+  handleEdit,
+  deleteEducationInfo,
+}) {
   return (
     <>
       <div id="form">
         <p className="personal-info-label">Education:</p>
-        <>
-          {!visibleForm ? (
-            <button id="formAppersButton" onClick={formAppers}>
-              +
+
+        {!visibleForm ? (
+          <button id="formAppersButton" onClick={makeFormAppear}>
+            +
+          </button>
+        ) : (
+          <>
+            <form id="personalForm">
+              <label htmlFor="school">School:</label>
+              <Form
+                name="school"
+                value={currentEducation.school || ""}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="degree">Degree:</label>
+              <Form
+                name="degree"
+                value={currentEducation.degree || ""}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="startDate">Start Date:</label>
+              <Form
+                name="startDate"
+                value={currentEducation.startDate || ""}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="endDate">End Date:</label>
+              <Form
+                name="endDate"
+                value={currentEducation.endDate || ""}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="location">Location:</label>
+              <Form
+                name="location"
+                value={currentEducation.location || ""}
+                onChange={handleInputChange}
+              />
+            </form>
+
+            <button
+              className="formButton"
+              id="saveFormButton"
+              onClick={saveEducationInfo}
+            >
+              Save
             </button>
-          ) : (
-            <>
-              <form id="personalForm">
-                <label htmlFor="school">School:</label>
-                <Form
-                  name="school"
-                  value={educationInfo.school}
-                  onChange={handleInputChange}
-                />
-                <label htmlFor="degree">Degree:</label>
-                <Form
-                  name="degree"
-                  value={educationInfo.degree}
-                  onChange={handleInputChange}
-                />
-                <label htmlFor="startDate">Start Date:</label>
-                <Form
-                  name="startDate"
-                  value={educationInfo.startDate}
-                  onChange={handleInputChange}
-                />
-                <label htmlFor="endDate">End Date:</label>
-                <Form
-                  name="endDate"
-                  value={educationInfo.endDate}
-                  onChange={handleInputChange}
-                />
-                <label htmlFor="location">Location:</label>
-                <Form
-                  name="location"
-                  value={educationInfo.location}
-                  onChange={handleInputChange}
-                />
-              </form>
-              <div id="buttonFormDiv">
-                <button id="deleteFormButton">Delete</button>
-                <button id="saveFormButton">Save</button>
-              </div>
-            </>
-          )}
-        </>
+          </>
+        )}
+
+        <div>
+          {educationList.map((school, index) => (
+            <div key={index} id="savedEducationInfo">
+              <p>School: {school.school}</p>
+              <p>Degree: {school.degree}</p>
+              <button id="editButton" onClick={() => handleEdit(index)}>
+                Edit
+              </button>
+              <button
+                id="deleteButton"
+                onClick={() => deleteEducationInfo(index)}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
