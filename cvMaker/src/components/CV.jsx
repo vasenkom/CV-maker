3e14e5c0ca5d58eb60f58d09b5ff46bfce13326e
vasenkom <m.vasenko@yandex.ru>
2024-09-23 19:37:18 +0200

    add displaying of job in cv part of application

--- cvMaker/src/components/CV.jsx
+++ cvMaker/src/components/CV.jsx
@@ -3,6 +3,9 @@ export function CVprototype({
   educationList,
   saveFormData,
   currentEducation,
+  jobList,
+  saveJobFormData,
+  currentJobInput,
 }) {
   return (
     <>
@@ -25,12 +28,11 @@ export function CVprototype({
         </div>
 
         <div id="education">
-          <h3 className="header-text">Education</h3>
-
           {/* Show both saved education and current form data if saveFormData is true */}
           {saveFormData && (
             <>
               {/* Show the current education being edited */}
+              <h3 className="header-text">Education</h3>
               <div className="education-entry">
                 <div id="column">
                   <p>
@@ -45,11 +47,13 @@ export function CVprototype({
               </div>
 
               {/* Show all saved education entries except the one being edited */}
+
               {saveFormData &&
                 educationList
                   .filter((edu) => !(edu.id === currentEducation.id))
                   .map((edu, index) => (
                     <div key={index} className="education-entry">
+                      <h3 className="header-text">Education</h3>
                       <div id="column">
                         <p>
                           {edu.startDate} - {edu.endDate}
@@ -69,6 +73,7 @@ export function CVprototype({
           {!saveFormData &&
             educationList.map((edu, index) => (
               <div key={index} className="education-entry">
+                <h3 className="header-text">Education</h3>
                 <div id="column">
                   <p>
                     {edu.startDate} - {edu.endDate}
@@ -78,6 +83,72 @@ export function CVprototype({
                 <div id="column">
                   <p>{edu.school}</p>
                   <p>{edu.degree}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+
+        {/* Job experience part */}
+        <div id="jobExperience">
+          {/* Show both saved education and current form data if saveFormData is true */}
+          {saveJobFormData && (
+            <>
+              {/* Show the current education being edited */}
+              <h3 className="header-text">Job experience</h3>
+              <div className="education-entry">
+                <div id="column">
+                  <p>
+                    {currentJobInput.startDateJob} -{" "}
+                    {currentJobInput.endDateJob}
+                  </p>
+                  <p>{currentJobInput.locationJob}</p>
+                </div>
+                <div id="column">
+                  <p>{currentJobInput.companyName}</p>
+                  <p>{currentJobInput.positionTitle}</p>
+                  <p>{currentJobInput.descriptionJob}</p>
+                </div>
+              </div>
+
+              {/* Show all saved education entries except the one being edited */}
+
+              {saveJobFormData &&
+                jobList
+                  .filter((job) => !(job.id === currentJobInput.idJob))
+                  .map((job, index) => (
+                    <div key={index} className="education-entry">
+                      <h3 className="header-text">Education</h3>
+                      <div id="column">
+                        <p>
+                          {job.startDateJob} - {job.endDateJob}
+                        </p>
+                        <p>{job.locationJob}</p>
+                      </div>
+                      <div id="column">
+                        <p>{job.companyName}</p>
+                        <p>{job.positionTitle}</p>
+                        <p>{job.descriptionJob}</p>
+                      </div>
+                    </div>
+                  ))}
+            </>
+          )}
+
+          {/* Show saved education entries if not editing */}
+          {!saveJobFormData &&
+            jobList.map((job, index) => (
+              <div key={index} className="education-entry">
+                <h3 className="header-text">Job Experience</h3>
+                <div id="column">
+                  <p>
+                    {job.startDateJob} - {job.endDateJob}
+                  </p>
+                  <p>{job.locationJob}</p>
+                </div>
+                <div id="column">
+                  <p>{job.companyName}</p>
+                  <p>{job.positionTitle}</p>
+                  <p>{job.descriptionJob}</p>
                 </div>
               </div>
             ))}
