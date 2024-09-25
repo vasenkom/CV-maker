b7a1b2469672c1f6a3505a7786f9b975d6c6bd97
vasenkom <m.vasenko@yandex.ru>
2024-09-25 20:03:06 +0200

    move css settings of toggle to css file

--- cvMaker/src/components/settings/ToggleSwitch.jsx
+++ cvMaker/src/components/settings/ToggleSwitch.jsx
@@ -3,15 +3,7 @@ import "./toggleSwitch.css";
 
 export function ToggleSwitch({ isChecked, stateMessage, toggleHandler }) {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        gap: "20px",
-        marginBottom: "30px",
-      }}
-    >
+    <div className="toggleDIV">
       <label className="switch">
         <input type="checkbox" checked={isChecked} onChange={toggleHandler} />
         <span className="slider round"></span>
