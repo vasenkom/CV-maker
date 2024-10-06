b254bff047bec40b5f71c3248bdd344bc01ff1f9
vasenkom <m.vasenko@yandex.ru>
2024-10-06 09:13:20 +0200

    add id to each div to fix css

--- cvMaker/src/components/forms/PersonalForms.jsx
+++ cvMaker/src/components/forms/PersonalForms.jsx
@@ -6,10 +6,30 @@ export function PersonalForms({
   customizeMode,
 }) {
   const formField = [
-    { label: "Name", name: "name", value: personalInfo.name },
-    { label: "Email", name: "email", value: personalInfo.email },
-    { label: "Phone number", name: "phone", value: personalInfo.phone },
-    { label: "Address", name: "address", value: personalInfo.address },
+    {
+      label: "Name",
+      name: "name",
+      value: personalInfo.name,
+      id: "personalForm",
+    },
+    {
+      label: "Email",
+      name: "email",
+      value: personalInfo.email,
+      id: "personalForm",
+    },
+    {
+      label: "Phone number",
+      name: "phone",
+      value: personalInfo.phone,
+      id: "personalForm",
+    },
+    {
+      label: "Address",
+      name: "address",
+      value: personalInfo.address,
+      id: "personalForm",
+    },
   ];
 
   return (
@@ -17,8 +37,8 @@ export function PersonalForms({
       <div id="form" style={{ display: customizeMode ? "none" : "block" }}>
         <p className="personal-info-label">Personal info:</p>
         <form id="personalForm">
-          {formField.map(({ label, name, value }, index) => (
-            <div key={index}>
+          {formField.map(({ label, name, value, id }, index) => (
+            <div key={index} id={id}>
               <label htmlFor={name}>{label}:</label>
               <Form name={name} value={value} onChange={handleInputChange} />
             </div>
